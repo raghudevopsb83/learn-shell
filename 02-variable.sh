17c3b8ae9f3f9196146256a84e9d46269eac8045
Raghu K <raghuk.vit@gmail.com>
2025-02-13 08:36:44 +0530

    RoboShop Code

--- 02-variable.sh
+++ 02-variable.sh
@@ -21,7 +21,7 @@ echo $a
 #   <Variable Input>   script     <Variable Input>
 
 #                  Env Variables
-#   left side input    script      Arguments
+#   LHS               script          RHS
 
 
 # 1. Envrionment variables
@@ -29,6 +29,14 @@ echo $a
 
 # on command line you run export x1=100 before executing this script
 echo x1 - $x1
+
+# 2. LHS
+# x2=200 script
+echo x2 - $x2
+
+# 2. RHS
+# script 300
+
 
 
 

e1f50b0795c0d12fee4d9275f9584866aab30537
Raghu K <raghuk.vit@gmail.com>
2025-02-13 07:43:06 +0530

    RoboShop Code

---
+++ 03-function.sh
@@ -0,0 +1,17 @@
+# If we assign a name to set of commands. it is called as function
+
+# Syntax:
+# function_name() {
+#   commands
+# }
+
+# Access the function
+# function_name
+
+# example
+example() {
+  echo Example Function
+}
+
+
+example

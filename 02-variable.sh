0959a6842f61e9a790e9cd463ace48b62139467e
Raghu K <raghuk.vit@gmail.com>
2025-02-13 08:33:04 +0530

    RoboShop Code

--- 02-variable.sh
+++ 02-variable.sh
@@ -15,3 +15,20 @@ echo $a
 # We can use characters a-z, A-Z
 # We can use numbers and Underscore
 
+
+# Input Variables
+#                < Variable Input>
+#   <Variable Input>   script     <Variable Input>
+
+#                  Env Variables
+#   left side input    script      Arguments
+
+
+# 1. Envrionment variables
+# export var=data
+
+# on command line you run export x1=100 before executing this script
+echo x1 - $x1
+
+
+

8e6001beed24e8ef3f6528b506154179a38732c4
Raghu K <raghuk.vit@gmail.com>
2025-02-13 08:41:57 +0530

    RoboShop Code

--- 03-function.sh
+++ 03-function.sh
@@ -21,3 +21,24 @@ example
 
 # variables in functions
 # In bash shell , if we declare a variable in main program, it is accessible inside function and vice-versa
+
+
+# Input Variables
+#                < Variable Input>
+#   <Variable Input>   function     <Variable Input>
+
+#               Variables / ENV variables
+#   LHS               function          RHS
+
+example1() {
+  echo Value of y - $y
+}
+
+y=200 example1
+
+example2() {
+  First Argument - $1
+}
+
+example2 300
+
